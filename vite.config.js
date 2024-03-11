8bc473272d70405f68aa3235698113a9978843aa
ROG\hamid <muhammad.hamid44@gmail.com>
2024-03-11 01:27:40 +0100

    Vite configuration for HTTPS added (comment out)

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  // server: {
+  //   https: true, // Not needed for Vite 5+
+  //   host: '127.0.0.1',
+  //   port: 5173
+  // },
+  plugins: [
+    // basicSsl({
+    //   /* name of certification */
+    //   name: 'api.com',
+    //   /** custom trust domains */
+    //   domains: ['api.com', '127.0.0.1', 'localhost']
+    //   /* custom certification directory */
+    //   // certDir: '/Users/.../.devServer/cert'
+    // }),
+    vue()
+  ],
   envPrefix: 'APP_',
   css: {
     postcss: {
